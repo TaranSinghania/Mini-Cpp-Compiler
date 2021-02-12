52610ff93c9c51442153c02f5fce9ed3009e9db3
Taran <42052166+TaranSinghania@users.noreply.github.com>
2021-02-12 20:25:08 +0530

    Add files via upload

---
+++ sample_program.cpp
@@ -0,0 +1,29 @@
+//This is a comment
+/*This is another comment*/
+
+#include <iostream.h>
+
+int main(){
+for(int i=0; i<10; i++){
+	int x = 10;
+}
+int a = -10.55;
+int b = 1;
+
+if(a == 0 && b != 5){
+	cout << "True" << endl;
+}
+
+
+if(b == 0){
+	cout << "False";
+}
+else{
+	cout << "True";
+}
+
+
+return 0;
+}
+
+
