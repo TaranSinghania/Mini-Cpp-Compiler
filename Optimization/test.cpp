de87078f0d4edc4603d0a2593fab2fa7e8339024
Rahul_Kata <katarahul@gmail.com>
2021-04-07 21:41:12 +0530

    Optimization

---
+++ Optimization/test.cpp
@@ -0,0 +1,34 @@
+//This is a comment
+/*This is another comment*/
+
+#include <iostream.h>
+
+int main()
+{
+    int a = 10;
+    int b = 20;
+    a++;
+
+    int c;
+    c = a + b;
+
+    int e = 10 + 100 * 100;
+
+    foo(a, b, c);
+
+    for (int i = 0; i < 100; i++)
+    {
+        int d = 12;
+    }
+
+    if (a == 10)
+    {
+        a += 15;
+    }
+    else
+    {
+        a *= 0;
+    }
+
+    return 0;
+}
